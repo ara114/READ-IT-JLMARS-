71af64e95d088a4dc3233eae4a5593fe9d4dc8c5
mma37 <mma37@hw.ac.uk>
2022-01-19 15:44:58 +0400

    Just removed stuff

--- src/App.js
+++ src/App.js
@@ -6,20 +6,20 @@ import SignUp from './pages/SignUp/Sign'
 import User from './pages/user/User'
 import Adventure from './pages/Adventure/Adventure'
 import './App.css'
-import {AuthProvider} from './contexts/AuthContext'
-import {Container} from 'react-bootstrap'
+// import { AuthProvider } from './contexts/AuthContext'
+
 function App() {
 	return (
 		<div className='App'>
-		<Routes>
-			<Route path='/' exact element={<LandingPage />} />
-			<Route path='/home' element={<Home />} />
-			<Route path='/login' element={<Login />} />
-			<Route path='/signup' element={<SignUp />} />
-			<Route path='/user' element={<User />} />
-			<Route path='/category/adventure' element={<Adventure />} />
-		</Routes>
-	</div>
+			<Routes>
+				<Route path='/' exact element={<LandingPage />} />
+				<Route path='/home' element={<Home />} />
+				<Route path='/login' element={<Login />} />
+				<Route path='/signup' element={<SignUp />} />
+				<Route path='/user' element={<User />} />
+				<Route path='/category/adventure' element={<Adventure />} />
+			</Routes>
+		</div>
 	)
 }
 
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,10 +4,6 @@ import { Link } from 'react-router-dom'
 function Footer() {
 	return (
 		<div className='footerContainer'>
-			{/* <section className='footerSub'>
-				<p className='footerSubHead'>Blah blah blah blah blah</p>
-				<p className='footerSubText'>DODODODDO</p>
-			</section> */}
 			<div className='footerLinks'>
 				<div className='footerLinkWrapper'>
 					<div className='footerlinkItems'>
@@ -28,19 +24,20 @@ function Footer() {
 						</Link>
 					</div>
 				</div>
-					<div className='footerlinkItems'>
-						<h2>Legal</h2>
-						<Link to='/Terms'>Terms</Link>
-						<Link to='/Privacy'>Privacy</Link>
-					</div>
+				<div className='footerlinkItems'>
+					<h2>Legal</h2>
+					<Link to='/Terms'>Terms</Link>
+					<Link to='/Privacy'>Privacy</Link>
+				</div>
 			</div>
 			<section className='socialMedia'>
 				<div className='socialWrap'>
 					<Link to='/' className='socialLogo'>
-						JLMARS <i className='fas fa-globe-europe'></i>
+						Read-it
+						{/* Read-it <i className='fas fa-globe-europe'></i> */}
 					</Link>
 
-					<small className='websiteRights'>JLMARS © 2022 </small>
+					<small className='websiteRights'>Read-it © 2022 </small>
 					<div className='socialIcons'>
 						<Link className='socialFacebook' to={{ pathname: '//facebook.com' }} target='_blank' aria-label='Facebook'>
 							<i className='fab fa-facebook-f' />
