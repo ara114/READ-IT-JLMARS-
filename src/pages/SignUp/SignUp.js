61641b515bc059a3c5929f8a8af54632d53d32a5
MahmoudAoude <71701098+MahmoudAoude@users.noreply.github.com>
2022-01-23 01:31:52 +0400

    minor fix for the sgnup responsivness.

--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -1,9 +1,9 @@
-import React, {useRef, useState} from 'react'
+import React, { useRef, useState } from 'react'
 import '../Login/Login'
-import {useAuth} from '../../contexts/AuthContext'
+import { useAuth } from '../../contexts/AuthContext'
 import Background from '../../components/backgroundImg/Background'
 import { Button } from '../../components/button/Button'
-function SignUp()  {
+function SignUp() {
 	// const emailRef = useRef();
 	// const passwordRef = useRef();
 	// const passwordConfirmRef = useRef();
@@ -27,39 +27,36 @@ function SignUp()  {
 	// 	setLoading(false)
 	// }
 	return (
-	
-		
-			<Background>
+		<Background>
 			<div className='card'>
 				<section className='loginContainer'>
-					<form >
+					<form>
 						<label>Email</label>
-						<input className='control' type='email' required placeholder='Email address'  />
+						<input className='control' type='email' required placeholder='Email address' />
 						<p className='errorMsg'></p>
-						
-							<label>Password</label>
-							<input className='control' placeholder="Password" type="password"required />
-							<p className='errorMsg'></p>
-							<label>Confirm Password</label>
-							<input className='control' placeholder="Confirm Password" type="password"  required/>
-							<p className='errorMsg'></p>
-							<section className='buttonsContainer'>
-							<Button type="submit" buttonStyle={'loginBtn'} buttonSize={'largeBtn'}>
+
+						<label>Password</label>
+						<input className='control' placeholder='Password' type='password' required />
+						<p className='errorMsg'></p>
+						<label>Confirm Password</label>
+						<input className='control' placeholder='Confirm Password' type='password' required />
+						<p className='errorMsg'></p>
+						<section className='buttonsContainer'>
+							<Button type='submit' buttonStyle={'loginBtn'} buttonSize={'largeBtn'}>
 								Signup
 							</Button>
-							<p className='msg'>Do you have an account?</p>
-							<Button buttonStyle={'loginBtn'} buttonSize={'LoginsmallBtn'}>
-							Login
-							</Button>
-							</section>
+							<div className='msgContainer'>
+								<p className='msg'>Do you have an account?</p>
+								<Button buttonStyle={'loginBtn'} buttonSize={'LoginsmallBtn'}>
+									Login
+								</Button>
+							</div>
+						</section>
 					</form>
 				</section>
 			</div>
 		</Background>
-
 	)
-
-
 }
 
 export default SignUp
