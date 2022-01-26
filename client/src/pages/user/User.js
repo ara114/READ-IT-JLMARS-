476ca34af52c400ceeef994df3ea0f21f7f3a521
MahmoudAoude <71701098+MahmoudAoude@users.noreply.github.com>
2022-01-27 01:06:50 +0400

    Done with the responsivness of the NavBar in the User.

--- client/src/pages/user/User.js
+++ client/src/pages/user/User.js
@@ -1,15 +1,19 @@
-
 import React from 'react'
 import Container from '../../components/container/container'
 import NavBar from '../../components/navbar/NavBar'
 import UserNav from '../user/UserNav'
+import Recommended from '../../components/Recommended/Recommended'
+import Categories from '../../components/Categories/Categories'
+import Write from '../../components/Write/Write'
 
 function User() {
-    return (
-        <Container nav={<UserNav/>}>
-			Hello
+	return (
+		<Container nav={<UserNav />}>
+			{/* <Recommended /> */}
+			<Categories />
+			<Write />
 		</Container>
-    )
+	)
 }
 
 export default User
--- client/src/pages/user/UserNav.js
+++ client/src/pages/user/UserNav.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import Categories from '../../components/Categories/Categories'
 import './UserNav.css'
-function UserNav(){
-    	// These states help in closing the shadow box
+function UserNav() {
+	// These states help in closing the shadow box
 	const [click, setClick] = useState(false)
 	const handleClick = () => setClick(!click)
 	const closeMobileMenu = () => setClick(false)
@@ -15,67 +16,53 @@ function UserNav(){
 		})
 	}
 
-	const [stickyClass, setStickyClass] = useState('')
+	return (
+		<header>
+			{/* <div className='tttttt'> */}
+			<nav className='userNav'>
+				{/* <div className='navbarContainer'> */}
 
-	useEffect(() => {
-		window.addEventListener('scroll', stickNavbar)
-		return () => window.removeEventListener('scroll', stickNavbar)
-	}, [])
+				<Link to='/home' className='userNavLogo' onClick={closeMobileMenu}>
+					{/* This is the logo with the name of the website, which can be found on the left */}
+					<div onClick={scrollToTop}>Readit</div>
+				</Link>
 
-	const stickNavbar = () => {
-		if (window !== undefined) {
-			let windowHeight = window.scrollY
-			// window height changed for the demo
-			windowHeight > 0 ? setStickyClass('sticky-nav') : setStickyClass('')
-		}
-	}
-
-        return (
-            <header>
-                <nav className={`userNav ${stickyClass}`}>
-                    {/* <div className='navbarContainer'> */}
-    
-                    <Link to='/home' className='userNavLogo' onClick={closeMobileMenu}>
-                        {/* This is the logo with the name of the website, which can be found on the left */}
-                        <div onClick={scrollToTop}>Readit</div>
-                    </Link>
-    
-                    <div className='icon' onClick={handleClick}>
-                        {/* This is the X and Hamburger logo on the right */}
-                        <i className={click ? 'fas fa-times' : 'fas fa-cog'} />
-                    </div>
-                    <ul className={click ? 'menu active' : 'menu'}>
-                        <li className='itemNav' onClick={scrollToTop}>
-                            {/* These are all the items in the nav bar */}
-                            <Link to='/home' className='itemLinks' onClick={closeMobileMenu}>
-                                <i className='fas fa-home'></i>
-                            </Link>
-                        </li>
-                        <li className='itemNav'>
-                            <Link to='/account' className='itemLinks' onClick={closeMobileMenu}>
-                            <i className="fas fa-user-circle"></i>
-                            </Link>
-                        </li>
-                        <li className='itemNav'>
-                            <Link to='/security' className='itemLinks' onClick={closeMobileMenu}>
-                            Security
-                            </Link>
-                        </li>
-                        <li className='itemNav'>
-                            <Link to='/About' className='itemLinks' onClick={closeMobileMenu}>
-                            About Us
-                            </Link>
-                        </li>
-                        <li className='itemNav'>
-                            <Link to='/Logout' className='itemLinks' onClick={closeMobileMenu}>
-                            Logout
-                            </Link>
-                        </li>
-                    </ul>
-                </nav>
-            </header>
-        ) 
+				<div className='icon' onClick={handleClick}>
+					{/* This is the X and Hamburger logo on the right */}
+					<i className={click ? 'fas fa-times' : 'fas fa-cog'} />
+				</div>
+				<ul className={click ? 'menu active' : 'menu'}>
+					<li className='itemNav' onClick={scrollToTop}>
+						{/* These are all the items in the nav bar */}
+						<Link to='/home' className='itemLinks' onClick={closeMobileMenu}>
+							<i className='fas fa-home'></i>
+						</Link>
+					</li>
+					<li className='itemNav'>
+						<Link to='/account' className='itemLinks' onClick={closeMobileMenu}>
+							<i className='fas fa-user-circle'></i>
+						</Link>
+					</li>
+					<li className='itemNav'>
+						<Link to='/security' className='itemLinks' onClick={closeMobileMenu}>
+							Security
+						</Link>
+					</li>
+					<li className='itemNav'>
+						<Link to='/About' className='itemLinks' onClick={closeMobileMenu}>
+							About Us
+						</Link>
+					</li>
+					<li className='itemNav'>
+						<Link to='/Logout' className='itemLinks' onClick={closeMobileMenu}>
+							Logout
+						</Link>
+					</li>
+				</ul>
+			</nav>
+			{/* </div> */}
+		</header>
+	)
 }
-
 
 export default UserNav
