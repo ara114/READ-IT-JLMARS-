fa213e5250a67e36da0c826d76d62c778892e709
MahmoudAoude <71701098+MahmoudAoude@users.noreply.github.com>
2022-01-29 16:53:07 +0400

    last update from the team.

--- client/src/Settings/Security/Security.js
+++ client/src/Settings/Security/Security.js
@@ -1,49 +1,50 @@
 import React from 'react'
+import Container from '../../components/container/container'
+import UserNav from '../../pages/user/UserNav'
 import './Security.css'
 
 export default function Security() {
+	return (
+		<Container nav={<UserNav />}>
+			<div className='sec-container'>
+				<div className='SecTitle'>
+					<h1>Security</h1>
+				</div>
+				<div className='form-div'>
+					<form>
+						<div className='input'>
+							<aside className='inp inp-label'>
+								<label htmlFor='old-pw'>Old password </label>
+							</aside>
+							<div className='inp inp-inp'>
+								<input type='password' id='old-pw' />
+							</div>
+						</div>
 
-    
+						<div className='input'>
+							<aside className='inp inp-label'>
+								<label htmlFor='new-pw'>New password </label>
+							</aside>
+							<div className='inp inp-inp'>
+								<input type='password' id='new-pw' />
+							</div>
+						</div>
 
-    return (
-        <div className='sec-container'>
-            <div>
-                <h1>Security</h1>
-            </div>
-            <div className="form-div">
-                <form>
-                    <div className='input'>
-                        <aside className="inp inp-label">
-                            <label htmlFor='old-pw'>Old password </label>
-                        </aside>
-                        <div className="inp inp-inp">
-                            <input type='password' id='old-pw' />
-                        </div>
-                    </div>
+						<div className='input'>
+							<aside className='inp inp-label'>
+								<label htmlFor='confirm-pw'>Confirm password </label>
+							</aside>
+							<div className='inp inp-inp'>
+								<input type='password' id='confirm-pw' />
+							</div>
+						</div>
 
-                    <div className='input'>
-                        <aside className="inp inp-label">
-                            <label htmlFor='new-pw'>New password </label>
-                        </aside>
-                        <div className="inp inp-inp">
-                            <input type='password' id='new-pw' />
-                        </div>
-                    </div>
-
-                    <div className='input'>
-                        <aside className="inp inp-label">
-                            <label htmlFor='confirm-pw'>Confirm password </label>
-                        </aside>
-                        <div className="inp inp-inp">
-                            <input type='password' id='confirm-pw' />
-                        </div>
-                    </div>
-
-                    <div>
-                        <button className='save-btn'>Save Password</button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    )
+						<div>
+							<button className='save-btn'>Save Password</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</Container>
+	)
 }
