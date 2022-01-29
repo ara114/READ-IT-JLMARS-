9a17209c5e62de90483aa181d7a660ac9bd0f997
MahmoudAoude <71701098+MahmoudAoude@users.noreply.github.com>
2022-01-29 16:52:40 +0400

    added the settings page changes.

--- client/src/Settings/Settings.js
+++ client/src/Settings/Settings.js
@@ -1,39 +1,27 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import "./Settings.css";
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './Settings.css'
 
 const Navbar = () => {
-    return (
-       
-         
-        
-        <nav className="navbar-setting border">
-            <h3 className= "SETTINGS">Settings</h3>       
-            <ul className="nav-links-setting border">
-            
-                <Link to="/account" className="account">
-                    <li>Account</li>
-                    </Link>
-                <Link to="/security" className="security">
-                    <li>Security</li>
-                    </Link>
-                <Link to="/about" className="about">
-                    <li>About</li>
-                    </Link>
-                <Link to="/logout" className="logout">
-                    <li>Logout</li>
-                    </Link>
+	return (
+		<nav className='navbar-setting border'>
+			<h3 className='SETTINGS'>Settings</h3>
+			<ul className='nav-links-setting border'>
+				<Link to='/account' className='account'>
+					<li>Account</li>
+				</Link>
+				<Link to='/security' className='security'>
+					<li>Security</li>
+				</Link>
+				<Link to='/about' className='about'>
+					<li>About</li>
+				</Link>
+				<Link to='/' className='logout'>
+					<li>Logout</li>
+				</Link>
+			</ul>
+		</nav>
+	)
+}
 
-
-
-
-            
-            
-            </ul>   
-        </nav> 
-          
-    );
-    
-    };
-      
-    export default Navbar;
+export default Navbar
