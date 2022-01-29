985fb6cfc0dd09ce62f12edb001df25436ea910c
MahmoudAoude <71701098+MahmoudAoude@users.noreply.github.com>
2022-01-29 16:52:17 +0400

    added the changes the team made and added made them responsive.

--- client/src/Settings/About/About.js
+++ client/src/Settings/About/About.js
@@ -1,28 +1,32 @@
 import React from 'react'
+import Container from '../../components/container/container'
+import UserNav from '../../pages/user/UserNav'
 import './About.css'
 
 export default function About() {
-    return (
-        <div className='abt-container'>
-            <div className='bg-img'>
-                <div className='page-title'>
-                    <h1>About Us</h1>
-                </div>
-            </div>
-            <div className='content-setting'>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </p>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </p>
-            </div>
-        </div>
-    )
+	return (
+		<Container nav={<UserNav />}>
+			<div className='abt-container'>
+				<div className='bg-img'>
+					<div className='page-title'>
+						<h1>About Us</h1>
+					</div>
+				</div>
+				<div className='content-setting'>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+						enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+						in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</p>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+						enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+						in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</p>
+				</div>
+			</div>
+		</Container>
+	)
 }
--- client/src/Settings/Account/Account.js
+++ client/src/Settings/Account/Account.js
@@ -1,75 +1,78 @@
 import React, { useRef } from 'react'
+import Container from '../../components/container/container'
+import UserNav from '../../pages/user/UserNav'
 import './Account.css'
 
 export default function Account() {
-    const nameInput = useRef()
-    const userNameInput = useRef()
-    const emailInput = useRef()
-    const dobInput = useRef()
-    const bioInput = useRef()
-    
-    function handleSubmit(event) {
-        event.preventDefault()
-    }
+	const nameInput = useRef()
+	const userNameInput = useRef()
+	const emailInput = useRef()
+	const dobInput = useRef()
+	const bioInput = useRef()
 
-    return (
-        <div className='acc-container'>
-            <div className='heading'>
-                <h1>Account</h1>
-            </div>
-            <div className="form-div">
-                <form onSubmit={handleSubmit}>
-                    
-                    <div className='input'>
-                        <aside className="inp inp-label">
-                            <label htmlFor='name'>Name </label>
-                        </aside>
-                        <div className="inp inp-inp">
-                            <input type='text' id='name' ref={nameInput} />
-                        </div>
-                    </div>
+	function handleSubmit(event) {
+		event.preventDefault()
+	}
 
-                    <div className='input'>
-                        <aside className="inp inp-label">
-                            <label htmlFor='user-name'>Username </label>
-                        </aside>
-                        <div className="inp inp-inp">
-                            <input type='text' id='user-name' ref={userNameInput} />
-                        </div>
-                    </div>
-                    
-                    <div className="input">
-                        <aside className="inp inp-label">
-                            <label htmlFor='email'>Email Address</label>
-                        </aside>
-                        <div className="inp inp-inp">
-                            <input type='email' id='email' ref={emailInput} />
-                        </div>
-                    </div>
-                    
-                    <div className="input">
-                        <aside className="inp inp-label">
-                            <label htmlFor='dob'>Date Of Birth </label>
-                        </aside>
-                        <div className="inp inp-inp">
-                            <input type='date' id='dob' ref={dobInput} />
-                        </div>
-                    </div>
-                    
-                    <div className="input">
-                        <aside className="inp inp-label">
-                            <label htmlFor='bio'>Bio </label>
-                        </aside>
-                        <div className="inp inp-inp">
-                            <textarea id='bio' rows='5' ref={bioInput} ></textarea>
-                        </div>
-                    </div>
-                    
-                    <div>
-                        <button className='save-btn'>Save Changes</button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    )
+	return (
+		<Container nav={<UserNav />}>
+			<div className='acc-container'>
+				<div className='heading'>
+					<h1>Account</h1>
+				</div>
+				<div className='form-div'>
+					<form onSubmit={handleSubmit}>
+						<div className='input'>
+							<aside className='inp inp-label'>
+								<label htmlFor='name'>Name </label>
+							</aside>
+							<div className='inp inp-inp'>
+								<input type='text' id='name' ref={nameInput} />
+							</div>
+						</div>
+
+						<div className='input'>
+							<aside className='inp inp-label'>
+								<label htmlFor='user-name'>Username </label>
+							</aside>
+							<div className='inp inp-inp'>
+								<input type='text' id='user-name' ref={userNameInput} />
+							</div>
+						</div>
+
+						<div className='input'>
+							<aside className='inp inp-label'>
+								<label htmlFor='email'>Email Address</label>
+							</aside>
+							<div className='inp inp-inp'>
+								<input type='email' id='email' ref={emailInput} />
+							</div>
+						</div>
+
+						<div className='input'>
+							<aside className='inp inp-label'>
+								<label htmlFor='dob'>Date Of Birth </label>
+							</aside>
+							<div className='inp inp-inp'>
+								<input type='date' id='dob' ref={dobInput} />
+							</div>
+						</div>
+
+						<div className='input'>
+							<aside className='inp inp-label'>
+								<label htmlFor='bio'>Bio </label>
+							</aside>
+							<div className='inp inp-inp'>
+								<textarea id='bio' rows='5' ref={bioInput}></textarea>
+							</div>
+						</div>
+
+						<div>
+							<button className='save-btn'>Save Changes</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</Container>
+	)
 }
