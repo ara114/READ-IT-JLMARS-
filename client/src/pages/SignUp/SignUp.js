598dabfad78da84728f9f2e1725230b95f296c5f
MahmoudAoude <71701098+MahmoudAoude@users.noreply.github.com>
2022-02-02 23:33:58 +0400

    Made it responsive after the changes that have been made

--- client/src/pages/SignUp/SignUp.js
+++ client/src/pages/SignUp/SignUp.js
@@ -6,8 +6,7 @@ import { Button } from '../../components/button/Button'
 import { Link } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
-import { signup } from '../../actions/auth';
-
+import { signup } from '../../actions/auth'
 
 function SignUp() {
 	// const emailRef = useRef();
@@ -33,62 +32,62 @@ function SignUp() {
 	// 	setLoading(false)
 	// }
 
-	const dispatch = useDispatch();
-	const navigate = useNavigate();
-	const initialState = {Nickname: '', email: '',password: '', ConfirmPassword: ''}
-	const [formData, setFormData] = useState(initialState);
+	const dispatch = useDispatch()
+	const navigate = useNavigate()
+	const initialState = { Nickname: '', email: '', password: '', ConfirmPassword: '' }
+	const [formData, setFormData] = useState(initialState)
 
 	const handleSubmit = (e) => {
-		e.preventDefault();
-		
-		dispatch(signup(formData, navigate));
-		
-	};
+		e.preventDefault()
+
+		dispatch(signup(formData, navigate))
+	}
 
 	const handleChange = (e) => {
 		setFormData({ ...formData, [e.target.name]: e.target.value })
-
-
-	};
+	}
 
 	return (
 		<div className='loginSignUpContainer'>
-		{' '}
+			{' '}
+			<div className='signCard'>
+				<section className='loginContainer'>
+					<form onSubmit={handleSubmit}>
+						<label>Nickname</label>
+						<input name='Nickname' className='control' required placeholder='Nickname' onChange={handleChange} />
 
-		<div className='signCard'>
-			<section className='loginContainer'>
-				<form onSubmit={handleSubmit}>
+						<label>Email</label>
+						<input name='email' className='control' type='email' required placeholder='Email address' onChange={handleChange} />
+						<p className='errorMsg'></p>
 
-					<label>Nickname</label>
-					<input name='Nickname' className='control' required placeholder='Nickname' onChange={handleChange}  />
-					
-					<label>Email</label>
-					<input name='email' className='control' type='email' required placeholder='Email address' onChange={handleChange} />
-					<p className='errorMsg'></p>
+						<label>Password</label>
+						<input name='password' className='control' placeholder='Password' type='password' required onChange={handleChange} />
+						<p className='errorMsg'></p>
 
-					<label>Password</label>
-					<input name='password' className='control' placeholder='Password' type='password' required onChange={handleChange} />
-					<p className='errorMsg'></p>
+						<label>Confirm Password</label>
+						<input
+							name='ConfirmPassword'
+							className='control'
+							placeholder='Confirm Password'
+							type='password'
+							required
+							onChange={handleChange}
+						/>
+						<p className='errorMsg'></p>
 
-					<label>Confirm Password</label>
-					<input name='ConfirmPassword'className='control' placeholder='Confirm Password' type='password' required onChange={handleChange} />
-					<p className='errorMsg'></p>
-					
-					<section className='signupBtn'>
-						<Button type='submit' buttonStyle={'loginBtn'} buttonSize={'largeBtn'}>
-							Signup
-						</Button>
-					</section>
-					<div className='SignmsgContainer'>
-						<p className='msg'>You're a member? </p>
-						<Link to='/login' style={{ textDecoration: 'none' }}>
-							<Button buttonStyle={'tester'} buttonSize={'LoginsmallBtn'}>
-								Login
+						<section className='signupBtn'>
+							<Button type='submit' buttonStyle={'loginBtn'} buttonSize={'largeBtn'}>
+								Signup
 							</Button>
-						</Link>
-					</div>
-				</form>
-			</section>
+						</section>
+						<div className='SignmsgContainer'>
+							<p className='msg'>You're a member? </p>
+							<Link className='linkText' to='/login' style={{ textDecoration: 'none' }}>
+								Login
+							</Link>
+						</div>
+					</form>
+				</section>
 			</div>
 		</div>
 	)
