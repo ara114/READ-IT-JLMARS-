6b84713f7cf441022209e31e5edb342fc044ec77
MahmoudAoude <71701098+MahmoudAoude@users.noreply.github.com>
2022-02-02 23:33:42 +0400

    Made a small change

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -2,39 +2,34 @@ import React, { useRef, useState } from 'react'
 import './Login.css'
 import { Button } from '../../components/button/Button'
 import { Link } from 'react-router-dom'
-import { login } from '../../actions/auth';
+import { login } from '../../actions/auth'
 import { useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 
-
 function Login(props) {
-
-	const dispatch = useDispatch();
-	const navigate = useNavigate();
-	const initialState = {email: '',password: ''}
-	const [formData, setFormData] = useState(initialState);
+	const dispatch = useDispatch()
+	const navigate = useNavigate()
+	const initialState = { email: '', password: '' }
+	const [formData, setFormData] = useState(initialState)
 
 	const handleSubmit = (e) => {
-		e.preventDefault();
-		
-		dispatch(login(formData, navigate));
-		
-	};
+		e.preventDefault()
+
+		dispatch(login(formData, navigate))
+	}
 
 	const handleChange = (e) => {
 		setFormData({ ...formData, [e.target.name]: e.target.value })
-
-
-	};
+	}
 
 	return (
 		<div className='loginSignUpContainer'>
 			<div className='card'>
 				<label>Email</label>
-				<input name='email' className='control' type='text' required placeholder='Email address' onChange={handleChange}/>
+				<input name='email' className='control' type='text' required placeholder='Email address' onChange={handleChange} />
 				<p className='errorMsg'></p>
 				<label>Password</label>
-				<input name='password' className='control' type='password' placeholder='Enter your password' required onChange={handleChange}/>
+				<input name='password' className='control' type='password' placeholder='Enter your password' required onChange={handleChange} />
 				<p className='errorMsg'></p>
 				<section className='loginButton'>
 					<Button buttonStyle={'loginBtn'} buttonSize={'largeBtn'} onClick={handleSubmit}>
@@ -43,10 +38,8 @@ function Login(props) {
 				</section>
 				<div className='msgContainer'>
 					<p className='msg'>Not a member?</p>
-					<Link to='/signup' style={{ textDecoration: 'none' }}>
-						<Button buttonStyle={'tester'} buttonSize={'LoginsmallBtn'}>
-							Signup
-						</Button>
+					<Link className='linkText' to='/signup' style={{ textDecoration: 'none', className: 'color' }}>
+						Signup
 					</Link>
 				</div>
 			</div>
