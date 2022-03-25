bb78905549ea990e2a587ed2441ad9d4e7b6e738
Mohammed Sadiq Bagalkot <mb200@hw.ac.uk>
2022-03-25 23:58:44 +0400

    Added dropdown menu to stories.

--- client/src/components/Stories/Story.js
+++ client/src/components/Stories/Story.js
@@ -1,18 +1,31 @@
-import './Story.css'
-import { Link } from 'react-router-dom'
-import { Button } from '@material-ui/core'
-import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt'
+import React, {useEffect, useState, useRef } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { Button } from '@material-ui/core';
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 import ThumbUpAltOutlined from '@material-ui/icons/ThumbUpAltOutlined';
-import DeleteIcon from '@material-ui/icons/Delete'
-import FlagIcon from '@mui/icons-material/Flag';
-import { useDispatch } from 'react-redux'
-import { deleteStory, likeStory, reportStory } from '../../actions/stories'
-import React, {useEffect, useState } from 'react';
+import DeleteIcon from '@material-ui/icons/Delete';
+import FlagOutlinedIcon from '@mui/icons-material/FlagOutlined';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import EditIcon from '@mui/icons-material/Edit';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import Popper from '@mui/material/Popper';
+import MenuItem from '@mui/material/MenuItem';
+import MenuList from '@mui/material/MenuList';
+import ClickAwayListener from '@mui/material/ClickAwayListener';
+import Grow from '@mui/material/Grow';
+import Paper from '@mui/material/Paper';
+import { useDispatch } from 'react-redux';
+import { deleteStory, likeStory, reportStory } from '../../actions/stories';
+import './Story.css';
+
 function Story({story}) {
 	const dispatch = useDispatch()
+	const navigate = useNavigate();
 	const user = JSON.parse(localStorage.getItem('profile'));
 	const [isReported, setIsReported] = useState(false);
 	const [likes, setLikes] = useState(story?.likes);
+	const [open, setOpen] = useState(false);
+	const anchorRef = useRef(null);
 
 	const scrollToTop = () => {
 		window.scrollTo({
@@ -20,6 +33,34 @@ function Story({story}) {
 			behavior: 'smooth',
 		})
 	}
+
+	const handleToggle = () => {
+		setOpen((prevOpen) => !prevOpen);
+	}
+
+	const handleClose = (event) => {
+		if (anchorRef.current && anchorRef.current.contains(event.target)) {
+		  return;
+		}
+	}
+
+	function handleListKeyDown(event) {
+		if (event.key === 'Tab') {
+		  event.preventDefault();
+		  setOpen(false);
+		} else if (event.key === 'Escape') {
+		  setOpen(false);
+		}
+	}
+
+	const prevOpen = useRef(open);
+	useEffect(() => {
+	  if (prevOpen.current === true && open === false) {
+		anchorRef.current.focus();
+	  }
+  
+	  prevOpen.current = open;
+	}, [open]);
 
 	const handleLike = async () => {
 		dispatch(likeStory(story.storyID));
@@ -52,12 +93,49 @@ function Story({story}) {
 							{likes.find((like) => like === (user?.result?.googleId || user?.result?._id)) ? (<ThumbUpAltIcon fontSize='small' className='likesBtn' />) : (<ThumbUpAltOutlined fontSize='small' className='likesBtn' />)}
 							&nbsp;{likes.length}
 						</Button>
-						<Button size='small' style={{ color: '#8e05c2' }} onClick={() => {setIsReported(true); dispatch(reportStory(story.storyID))}}>
-							<FlagIcon fontSize='small' className='deleteBtn' />
+						<Button
+							ref={anchorRef}
+							id="composition-button"
+							aria-controls={open ? 'composition-menu' : undefined}
+							aria-expanded={open ? 'true' : undefined}
+							aria-haspopup="true"
+							onClick={handleToggle}
+							>
+							<MoreHorizIcon/>
 						</Button>
-						<Button size='small' style={{ color: '#8e05c2' }} onClick={() => dispatch(deleteStory(story.storyID))}>
-							<DeleteIcon fontSize='small' className='deleteBtn' />
-						</Button>
+						<Popper
+							open={open}
+							anchorEl={anchorRef.current}
+							role={undefined}
+							placement="bottom-start"
+							transition
+							disablePortal
+							>
+							{({ TransitionProps, placement }) => (
+								<Grow
+								{...TransitionProps}
+								style={{
+									transformOrigin:
+									placement === 'bottom-start' ? 'left top' : 'left bottom',
+								}}
+								>
+								<Paper>
+									<ClickAwayListener onClickAway={handleClose}>
+									<MenuList
+										autoFocusItem={open}
+										id="composition-menu"
+										aria-labelledby="composition-button"
+										onKeyDown={handleListKeyDown}
+									>
+										<MenuItem onClick={() => {navigate(`/Create/${story.storyID}`);}}><EditIcon fontSize='small' style={{ color: '#8e05c2' }}/>&nbsp;Edit</MenuItem>
+										<MenuItem onClick={(e) => {setIsReported(true); dispatch(reportStory(story.storyID))}}><FlagOutlinedIcon fontSize='small' style={{ color: '#8e05c2' }} className='deleteBtn' />&nbsp;Report</MenuItem>
+										<MenuItem onClick={() => dispatch(deleteStory(story.storyID))}><DeleteIcon fontSize='small' style={{ color: 'red' }} className='deleteBtn' />&nbsp;Delete</MenuItem>
+									</MenuList>
+									</ClickAwayListener>
+								</Paper>
+								</Grow>
+							)}
+						</Popper>
 					</div>
 				</div>
 			</div>
