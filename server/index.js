f8c91bc28543bee484257c7e58c20d75c1aef4e9
Mohammed Sadiq Bagalkot <mb200@hw.ac.uk>
2023-05-20 16:48:39 +0400

    make it better

--- server/index.js
+++ server/index.js
@@ -140,6 +140,10 @@ app.use('/stories', storyRoutes);
 app.use('/users', userRoutes);
 app.use('/moderator', moderatorRoutes);
 
+app.get('/', (req, res) => {
+    res.send('Hello to ReadIt API');
+})
+
 const PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => server.listen(PORT, () => console.log(`Server running on port ${PORT}`))) // if successful connection
